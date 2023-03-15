76b0c7fab2de890d5ed7ffb7e9fbb3c4125d844b
alexsandro cubas <alexsandro.cubas@pneufree.com.br>
2023-03-15 14:06:07 -0300

    eu quero ver se funciona porra

---
+++ src/routes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const routes = express.Router();
+
+const VideoController = require("./controllers/VideoController");
+
+// routes.get("/", (request, response) => response.send("Hello World"));
+
+routes.get("/videos", VideoController.index);
+routes.post("/videos", VideoController.store);
+
+routes.get("/", (req, res) => {
+    res.send("deu boa");
+});
+
+module.exports = routes;
---
+++ src/server.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+
+const express = require("express");
+const routes = require("./routes");
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+const port = 3333;
+
+app.listen(port, () => {
+  console.log(`backend started at http://localhost:${port}`);
+});
+
+const connectToDataBase = require("./database");
+
+connectToDataBase();
